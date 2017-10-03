f090be9b2ed1a42e863d166738802f6c0d4f4a8f
Voikgrib <32133129+Voikgrib@users.noreply.github.com>
2017-10-03 23:31:51 +0300

    Add files via upload

--- Stack_system.cpp
+++ Stack_system.cpp
@@ -3,22 +3,28 @@
 #include<assert.h>
 #include<string.h>
 
+typedef int data_type;
+
 const long int Max_size_of_my_stack = 666;
+const int Poizon_num = '\0';
 
-int stack_construct(struct t_my_stack *my_stack);
-int my_push(struct t_my_stack *my_stack, int num);
-int my_pop(struct t_my_stack *my_stack);
+int Err_code = 0;
 
+data_type my_pop(struct s_my_stack *my_stack);
+int is_stack_ok(struct s_my_stack *my_stack);
+
+void stack_construct(struct s_my_stack *my_stack);
+void my_push(struct s_my_stack *my_stack, data_type num);
 void com_cleaner(char* command, int com_size);
-void err_print(int err_code);
+void err_print(void);
 
 //!------------------------------------------------------------------------------------------
-//! Declare structure t_my_stack
+//! Declare structure s_my_stack
 //!
 //! @param char data[] - massif with stack data
 //! @param int counter - pointer on current pozition
 //!------------------------------------------------------------------------------------------
-struct t_my_stack
+struct s_my_stack
 {
     char data [Max_size_of_my_stack];
     int counter;
@@ -33,15 +39,14 @@ int main()
 {
     const int max_comm_lenght = 100;
 
-    int err = 0;
     char cur_command[max_comm_lenght] = {0};
-    int num = 0;
+    data_type num = 0;
 
-    struct t_my_stack stack_1;
+    struct s_my_stack stack_1;
 
     printf("Stack created!\n");
 
-    err = stack_construct(&stack_1);
+    stack_construct(&stack_1);
 
     while(strcmp(cur_command,"exit") != 0)
     {
@@ -52,7 +57,7 @@ int main()
         if(strcmp(cur_command,"push") == 0)
         {
             scanf("%d", &num);
-            err = my_push(&stack_1, num);
+            my_push(&stack_1, num);
         }
         else if(strcmp(cur_command,"pop") == 0)
         {
@@ -60,23 +65,30 @@ int main()
             printf("num = %d \n", num);
         }
 
-        if(err != 0)
-            err_print(err);
+        if(Err_code != 0)
+        {
+            err_print();
+            Err_code = 0;
+        }
     }
 
-    return err;
+    return Err_code;
 }
 
 //!------------------------------------------------------------------------------------------
 //! This function initialize stack: fulls data[] = {}, set counter = 0
 //!
-//! @param[in] struct t_my_stack *my_stack - stack which will be initialized
+//! @param[in] struct s_my_stack *my_stack - stack which will be initialized
 //!
-//! @param[out] 0 - if no errors
+//! @note Error codes 6 - if memory or pointer error before construct
+//!                   7 - if memory or pointer error after construct
 //!------------------------------------------------------------------------------------------
-int stack_construct(struct t_my_stack *my_stack)
+void stack_construct(struct s_my_stack *my_stack)
 {
     int i = 0;
+
+    if(is_stack_ok(my_stack) == -1)
+        Err_code = 6;
 
     while(i != Max_size_of_my_stack)
     {
@@ -86,20 +98,26 @@ int stack_construct(struct t_my_stack *my_stack)
 
     my_stack->counter = 0;
 
-    return 0;
+    if(is_stack_ok(my_stack) == -1)
+        Err_code = 7;
+
 }
 
 //!------------------------------------------------------------------------------------------
 //! This function put number in stack
 //!
-//! @param[in] struct t_my_stack *my_stack - stack we are working on
+//! @param[in] struct s_my_stack *my_stack - stack we are working on
 //! @param[in] int num - number which will be put in stack
 //!
-//! @param[out] 0 - if no errors
-//! @param[out] 1 - if stack is full
+//! @note Error codes 1 - if stack is full
+//!                   2 - memory or pointer error before push
+//!                   3 - memory or pointer error after push
 //!------------------------------------------------------------------------------------------
-int my_push(struct t_my_stack *my_stack, int num)
+void my_push(struct s_my_stack *my_stack, data_type num)
 {
+    if(is_stack_ok(my_stack) == -1)
+        Err_code = 2;
+
     if(my_stack->counter != Max_size_of_my_stack)
     {
         my_stack->data[my_stack->counter] = num;
@@ -107,40 +125,50 @@ int my_push(struct t_my_stack *my_stack, int num)
     }
     else
     {
-        return 1;
+        Err_code = 1;
     }
 
-    return 0;
+    if(is_stack_ok(my_stack) == -1)
+        Err_code = 3;
 }
 
 //!------------------------------------------------------------------------------------------
 //! This function get number from stack
 //!
-//! @param[in] struct t_my_stack *my_stack - stack we are working on
+//! @param[in] struct s_my_stack *my_stack - stack we are working on
 //!
-//! @param[out] 0 - if no errors
-//! @param[out] -1 & error_message - stack is empty
+//! @note Error codes 4 - memory or pointer error before pop
+//!                   5 - memory or pointer error after pop
+//!                   -1 - stack is empty
+//!
+//! @param[out] data_type num - number (or something) from stuck
 //!------------------------------------------------------------------------------------------
-int my_pop(struct t_my_stack *my_stack)
+data_type my_pop(struct s_my_stack *my_stack)
 {
     int num = 0;
+
+    if(is_stack_ok(my_stack) == -1)
+        return 4;
 
     if(my_stack->counter > 0)
     {
         num = my_stack->data[--my_stack->counter];
-
-        if(my_stack->counter == 0)
-            printf("### Alarm! ###  Stack is empty mow! You can't get more numbers :c \n");
-
-
+        my_stack->data[my_stack->counter] = Poizon_num;
         return num;
 
     }
-    else
+    else if(is_stack_ok(my_stack) <= -1)
     {
-        printf("### Error! ###  You can't do this. Stack is empty! (return -1) meow :3 \n");
+        Err_code = 5;
         return -1;
     }
+    else
+    {
+        Err_code = -1;
+        return -1;
+    }
+
+
 }
 
 //!------------------------------------------------------------------------------------------
@@ -155,7 +183,7 @@ void com_cleaner(char* command, int com_size)
 
     while(i != com_size)
     {
-        command[i] = '\0';
+        command[i] = Poizon_num;
         i++;
     }
 }
@@ -163,10 +191,41 @@ void com_cleaner(char* command, int com_size)
 //!------------------------------------------------------------------------------------------
 //! This function prints error messages about last error
 //!
-//! @param[in] err_code - code of last error
+//! @param Err_code - code of last error
 //!------------------------------------------------------------------------------------------
-void err_print(int err_code)
+void err_print()
 {
-    if(err_code == 1)
-        printf("!!!  ERROR_PRINT - END_OF_MEMORY  (err_code = %d) !!!\n\n", err_code);
+    if(Err_code == -1)
+        printf("!!!  ERROR_PRINT - STACK_IS_EMPTY  (Err_code = %d) !!!\n\n", Err_code);
+    else if(Err_code == 1)
+        printf("!!!  ERROR_PRINT - END_OF_MEMORY  (Err_code = %d) !!!\n\n", Err_code);
+    else if(Err_code == 2)
+        printf("!!!  ERROR_PRINT - ERROR_IN_PUSH_START  (Err_code = %d) !!!\n\n", Err_code);
+    else if(Err_code == 3)
+        printf("!!!  ERROR_PRINT - ERROR_IN_PUSH_END  (Err_code = %d) !!!\n\n", Err_code);
+    else if(Err_code == 4)
+        printf("!!!  ERROR_PRINT - ERROR_IN_POP_START  (Err_code = %d) !!!\n\n", Err_code);
+    else if(Err_code == 5)
+        printf("!!!  ERROR_PRINT - ERROR_IN_PUSH_END  (Err_code = %d) !!!\n\n", Err_code);
+    else if(Err_code == 6)
+        printf("!!!  ERROR_PRINT - ERROR_IN_CREARE_STACK_START  (Err_code = %d) !!!\n\n", Err_code);
+    else if(Err_code == 5)
+        printf("!!!  ERROR_PRINT - ERROR_IN_CREARE_STACK_END  (Err_code = %d) !!!\n\n", Err_code);
+
+}
+
+//!------------------------------------------------------------------------------------------
+//! This function check stack for errors
+//!
+//! @param[in] struct s_my_stack *my_stack - stack which we check
+//!------------------------------------------------------------------------------------------
+int is_stack_ok(struct s_my_stack *my_stack)
+{
+    if(my_stack &&
+       my_stack->counter <= Max_size_of_my_stack &&
+       my_stack->counter >= 0 &&
+       my_stack->data)
+        return 0;
+    else
+        return -1;
 }
