7b8f31697ca660ede0df7a6b33db51a5fab3a07f
Voikgrib <32133129+Voikgrib@users.noreply.github.com>
2017-10-20 01:13:07 +0300

    Add files via upload

---
+++ my_assembly_command_info.h
@@ -0,0 +1,23 @@
+#ifndef MY_ASSEMBLY_COMMAND_INFO_H_INCLUDED
+#define MY_ASSEMBLY_COMMAND_INFO_H_INCLUDED
+
+//!----------------------------------------------------------------------------------
+//! Main information about function v 1.3
+//!
+//! ( Say NO to copy-paste! )
+//!
+//! Author: Vladimir Gribanov
+//!----------------------------------------------------------------------------------
+
+const int Push = 1;
+const int Pop = 2;
+const int Add = 3;
+const int Mul = 4;
+const int Sub = 5;
+const int Div = 6;
+const int Jum = 7;
+const int Push_reg = 8;
+const int Pop_reg = 9;
+const int End = 0;
+
+#endif // MY_ASSEMBLY_COMMAND_INFO_H_INCLUDED
